aa5dee3729a58491e1fd8b6ccea322b4a7a03203
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2024-06-22 17:55:12 +0200

    webpage with details

---
+++ webPage/PHP/sign.php
@@ -0,0 +1,18 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+    try {
+        require_once("db_handler.php"); //lenyegeben linkeles, hasonloak: require, include, include_once
+        $query = "INSERT INTO `library`.`user` (`username`,`password`,`email`)VALUES('{$_POST['username']}', '{$_POST['password']}','{$_POST['email']}');";  
+        $stmt = $pdo -> prepare($query);
+        
+        $stmt -> execute();
+        
+        echo json_encode("ok");
+
+        $pdo = null;
+        $stmt = null;
+    } catch (\Throwable $th) {
+        echo json_encode("nemok");
+    }
+  
+
